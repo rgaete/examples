723011d9216b8339a80d57790c7ecba763ee4e9f
Ricardo Gaete Duarte <ricardo.gaete@latam.com>
2018-06-03 14:38:04 -0400

    First commit

---
+++ complexity/src/main/java/com/rgfp/scrumdeveloper/complexity/ComplexityApplication.java
@@ -0,0 +1,12 @@
+package com.rgfp.scrumdeveloper.complexity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplexityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComplexityApplication.class, args);
+	}
+}
