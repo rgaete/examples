95e27429c7344d2a2ed0d3f7d6428849729a3279
Fernando Ignacio Poblete Arrau <fernando.poblete@latam.com>
2018-06-03 23:31:21 -0400

    Adding email imap service so later we can do a pull up refactoring

---
+++ refactoring/src/main/java/com/rgfp/scrumdeveloper/messageHandling/EmailImapService.java
@@ -0,0 +1,11 @@
+package com.rgfp.scrumdeveloper.messageHandling;
+
+public class EmailImapService {
+
+    // incomplete, implemented just for the refactoring exercises
+
+    public void send(String message) {
+        // do nothing
+    }
+
+}
