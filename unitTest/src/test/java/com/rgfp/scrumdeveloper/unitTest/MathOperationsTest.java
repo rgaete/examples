1fc831f82f6a2db39ec4a40f5a0fc6a9a56b5a06
Fernando Ignacio Poblete Arrau <fernando.poblete@latam.com>
2018-06-05 18:43:58 -0400

    Removing all but one test for exercising

--- unitTest/src/test/java/com/rgfp/scrumdeveloper/unitTest/MathOperationsTest.java
+++ unitTest/src/test/java/com/rgfp/scrumdeveloper/unitTest/MathOperationsTest.java
@@ -22,85 +22,12 @@ public class MathOperationsTest {
 
     @Test
     public void itShouldSumZeroForAnEmptyArray() {
-        Integer[] numbers = {};
 
-        Integer result = MathOperations.sum(numbers);
+        // Arrange
 
-        assertEquals(new Integer(0), result);
-    }
+        // Act
 
-    @Test
-    public void itShouldReturnTheMaxValueOfAnArray() {
-        Integer[] numbers = {1,2,3};
-
-        Integer result = MathOperations.max(numbers);
-
-        assertEquals(new Integer(3), result);
-    }
-
-    @Test
-    public void itShouldReturnAMaxOfNullForAnEmptyArray() {
-        Integer[] numbers = {};
-
-        Integer result = MathOperations.max(numbers);
-
-        assertNull(result);
-    }
-
-    @Test
-    public void itShouldReturnTheMinValueOfAnArray() {
-        Integer[] numbers = {1,2,3};
-
-        Integer result = MathOperations.min(numbers);
-
-        assertEquals(new Integer(1), result);
-    }
-
-    @Test
-    public void itShouldReturnAMinOfNullForAnEmptyArray() {
-        Integer[] numbers = {};
-
-        Integer result = MathOperations.min(numbers);
-
-        assertNull(result);
-    }
-
-    @Test
-    public void itShouldReturnTheAvgValueOfAnArray() {
-        Integer[] numbers = {1,2,1};
-
-        Float result = MathOperations.average(numbers);
-
-        assertEquals(new Float(1.3333333), result, 0.000001f);
-    }
-
-    @Test
-    public void itShouldReturnAnAvgOfNullForAnEmptyArray() {
-        Integer[] numbers = {};
-
-        Float result = MathOperations.average(numbers);
-
-        assertNull(result);
-    }
-
-    @Test
-    public void itShouldTheLengthOfAnArray() {
-
-        Integer[] numbers = {1,2,3};
-
-        Integer result = MathOperations.length(numbers);
-
-        assertEquals(new Integer(3), result);
-
-    }
-
-    @Test
-    public void itShouldReturnALengthOfZeroForAnEmptyArray() {
-        Integer[] numbers = {};
-
-        Integer result = MathOperations.length(numbers);
-
-        assertEquals(new Integer(0), result);
+        // Assert
     }
 
 }
