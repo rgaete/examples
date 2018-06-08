c337744a2ece264fb6830b64a8ae8f0664270db7
Fernando Ignacio Poblete Arrau <fernando.poblete@latam.com>
2018-06-08 02:24:39 -0400

    Adding a tdd simple exercise

---
+++ tdd/src/main/java/com/rgfp/scrumdeveloper/tdd/TDDApplication.java
@@ -0,0 +1,15 @@
+package com.rgfp.scrumdeveloper.tdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TDDApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TDDApplication.class, args);
+
+		StringCalculator.add("");
+
+	}
+}
